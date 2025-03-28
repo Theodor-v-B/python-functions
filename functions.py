3fe7183338a3122052672324d3f44cfcc77af181
Theodor-v-B <tgvonbrentano@hotmail.com>
2025-03-28 10:42:58 +0100

    Optimized the greeting functionality in the app by adding the greet() function.

--- functions.py
+++ functions.py
@@ -1,9 +1,14 @@
+def greet():
+    print("Hello, World!")
+
+# some other code
+
+print("Other Code 1.")
+
 print("Hello, World!")
 
 # some other code
 
-print("Hello, World!")
-
-# some other code
+print("Other Code 2.")
 
 print("Hello, World!")
